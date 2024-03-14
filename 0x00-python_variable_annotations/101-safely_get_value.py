2991fb9b6ef2b965bb60ded8c3b8f038cc5b2a1f
wisdomwest <harmonykoome@gmail.com>
2024-03-14 22:10:21 +0300

    pycode error

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -6,7 +6,10 @@ from typing import Mapping, Any, Union, TypeVar
 
 T = TypeVar('T')
 
-def safely_get_value(dct: Mapping, key: Any, default: Union[T, None] = None) -> Union[Any, T]:
+
+def safely_get_value(dct: Mapping, key: Any,
+                     default: Union[T, None] = None
+                     ) -> Union[Any, T]:
     '''Get value from dictionary'''
     if key in dct:
         return dct[key]
